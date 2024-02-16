5899174b191424ce421fe7751b19dc8d6180074f
Tars Verschelde <tars.verschelde@kulueven.be>
2024-02-16 15:56:01 +0100

    move files from data tools to specific repository

---
+++ convert_ines_pypsa.py
@@ -0,0 +1,136 @@
+def map_preprocess(iodb):
+	# instead choose an intermediate format that is closer to spine?
+	# e.g. [] instead of {}?
+	iodb = iodb | {
+			"Bus" : {},
+			"Generator" : {},
+			"Link" : {},
+			"Load" : {}
+		}
+	return
+
+def map_postprocess(iodb):
+	#filter out None values
+	for (entitytype,entities) in iodb.items():
+		for (entityname,entitityattributes) in entities.items():
+			for (attribute,value) in entitityattributes.items():
+				if value == None:
+					iodb[entitytype][entityname].pop(attribute)
+	return
+
+# Function map for entity classes; specific to the generic structure
+def map_constraint(iodb,entities,parameters):
+	return
+
+def map_link(iodb,entities,parameters):
+	entityname = entities[1]
+	parameter = parameters[1]
+	iodb["Link"] = iodb["Link"].get(entityname,{}) | {
+		"efficiency" : 1,
+		"marginal_cost" : 0.0,
+		"p_min_pu" : -1,
+		"p_nom" : parameter["capacity"]
+	}
+	return
+
+def map_node(iodb,entities,parameters):
+	# can be a node, load or source
+	entityname = entities[1]
+	parameter = parameters[1]
+	iodb["Bus"].get(entityname,{})
+	iodb["Load"] = iodb["Load"].get("load "*entityname, {}) | {
+		"bus" : entityname,
+		"p_set" : parameter["demand_profile"]
+	}
+	iodb["Generator"] = iodb["Generator"].get("generator "*entityname, {}) | {
+		"bus" : entityname,
+		"marginal_cost" : parameter["commodity_price"],
+		"p_nom" : parameter["upper_limit"]
+	}
+	return
+
+def map_period(iodb,entities,parameters):
+	return
+
+def map_set(iodb,entities,parameters):
+	return
+
+def map_solve_pattern(iodb,entities,parameters):
+	return
+
+def map_system(iodb,entities,parameters):
+	return
+
+def map_temporality(iodb,entities,parameters):
+	return
+
+def map_tool(iodb,entities,parameters):
+	return
+
+def map_unit(iodb,entities,parameters):
+	entityname = entities[1]
+	parameter = parameters[1]
+
+	iodb["Link"] = iodb["Link"].get(entityname, {}) | {
+		"efficiency" : parameter["efficiency"]
+	}
+	return
+
+def map_node__to_unit(iodb,entities,parameters):
+	entityname = entities[2]
+	parameter = parameters[1]
+	iodb["Link"] = iodb["Link"].get(entityname, {}) | {
+		"efficiency" : parameter["conversion_coefficient"],
+		"marginal_cost" : parameter["other_operational_cost"],
+		"p_nom" : parameter["capacity_per_unit"]
+	}
+	return
+
+def map_set__link(iodb,entities,parameters):
+	return
+
+def map_set_node(iodb,entities,parameters):
+	return
+
+def map_set_temporality(iodb,entities,parameters):
+	return
+
+def map_set__unit(iodb,entities,parameters):
+	return
+
+def map_tool_set(iodb,entities,parameters):
+	return
+
+def map_unit__to_node(iodb,entities,parameters):
+	entityname = entities[2]
+	busname = entities[3]
+	parameter = parameters[1]
+	iodb["Link"] = iodb["Link"].get(entityname, {}) | {
+		"bus1" : busname,
+		"marginal_cost" : parameter["other_operational_cost"],
+		"p_nom" : parameter["capacity_per_unit"]
+	}
+	return
+
+def map_node__link__node(iodb,entities,parameters):
+	entityname = entities[1]
+	busname0 = entities[2]
+	busname1 = entities[4]
+
+	iodb["Link"] = iodb["Link"].get(entityname, {}) | {
+		"bus0" : busname0,
+		"bus1" : busname1
+	}
+	return
+
+def map_set__node__temporality(iodb,entities,parameters):
+	return
+
+def map_set__node__unit(iodb,entities,parameters):
+	entityname = entities[3]
+	busname = entities[2]
+
+	iodb["Link"] = iodb["Link"].get(entityname, {}) | {
+		"bus1" : busname
+	}
+	return
---
+++ convert_pypsa_ines.py
@@ -0,0 +1,54 @@
+import sys
+import json
+#import spinedb_api as api
+#from pathlib import Path
+
+#path = Path(__file__).resolve().parent
+#ines = ARGS[1] # use template to verify valid fields?
+#map = ARGS[2] # use map to make a general structure like the exporters?
+ARGS = sys.argv[1:]
+input = ARGS[1]
+output = ARGS[2]
+
+with open(input) as f:
+    pypsadict = json.load(f)
+
+iodb = {
+    "entities" : [],
+    "parameter_values" : [],
+    "alternatives" : [["PyPSA", ""]]
+}
+
+#Bus => node
+for (name,parameters) in pypsadict["Bus"].items():
+    iodb["entities"].append(["node", "bus "*name, [], None])
+
+#Generator => node
+for (name,parameters) in pypsadict["Generator"].items():
+    iodb["entities"].append(["node", "gen "*name, [], None])
+    iodb["parameter_values"].append(["node", "gen "*name, "commodity_price", parameters["marginal_cost"], "PyPSA"])
+    iodb["parameter_values"].append(["node", "load "*name, "capacity_per_unit", parameters["p_nom"], "PyPSA"])
+
+#Load => node
+for (name,parameters) in pypsadict["Load"].items():
+    iodb["entities"].append(["node", "load "*name, [], None])
+    iodb["parameter_values"].append(["node", "load "*name, "demand_profile", parameters["p_set"], "PyPSA"])
+
+#Link => link or node
+for (name,parameters) in pypsadict["Link"].items():
+    if parameters["efficiency"]==1 and parameters["marginal_cost"]==0.0 and parameters["p_min_pu"]==-1:
+        iodb["entities"].append(["link", "link "*name, [], None])
+        iodb["entities"].append(["node__link__node", parameters["bus0"]*"_"*name*"_"*parameters["bus1"], [parameters["bus0"], name, parameters["bus1"]], None])
+        iodb["parameter_values"].append(["link", "link "*name, "capacity", parameters["p_nom"], "PyPSA"])
+        iodb["parameter_values"].append(["link", "link "*name, "efficiency", 1.0, "PyPSA"])
+    else:
+        iodb["entities"].append(["node__to_unit", parameters["bus0"]*"_"*name, ["bus0",name], None])
+        iodb["entities"].append(["unit", "link "*name, [], None])
+        iodb["entities"].append(["unit__to_node", name*"_"*parameters["bus1"], [name,"bus1"], None])
+        iodb["parameter_values"].append(["unit", "link "*name, "efficiency", parameters["efficiency"], "PyPSA"])
+        iodb["parameter_values"].append(["unit__to_node", ["link "*name, parameters["bus1"]], "other_operational_cost", parameters["marginal_cost"], "PyPSA"])
+        iodb["parameter_values"].append(["unit__to_node", ["link "*name, parameters["bus1"]], "capacity_per_unit", parameters["p_nom"], "PyPSA"])
+
+#SpineInterface.import_data(output, iodb, "import data from PyPSA")
+with open(output, 'w') as f:
+    json.dump(iodb, f, indent=4)
